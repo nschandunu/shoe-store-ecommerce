3b387b9710eeea9bcb5a9e430931a167f6075791
Senuka Chandunu <senukaacc@gmail.com>
2024-12-31 11:57:26 +0530

    Mobile responsive nav bar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -5,7 +5,6 @@ import { FaBarsStaggered, FaRegCircleUser } from 'react-icons/fa6'
 import { FaSearch } from 'react-icons/fa'
 import {TbBasket, TbUserCircle} from 'react-icons/tb'
 import { RiUserLine } from 'react-icons/ri'
- 
 
 
 const Header = () => {
@@ -13,6 +12,8 @@ const Header = () => {
 
     const [token, settoken] = useState('')
     const [menuOpened, setMenuOpened] = useState(false)
+
+    const toggleMenu = () => setMenuOpened((prev) => !prev)
 
     return (
    <header className='max-padd-container w-full z-50'>
@@ -24,22 +25,23 @@ const Header = () => {
                 </div>
             </Link>
 
-            {/*Navbar */}
+            {/* Navbar */}
             <div className='flex-1'>
-                <Navbar />
-
+                <Navbar containerStyles={`${menuOpened ?
+                "flex items-start flex-col gap-y-8 fixed top-16 right-6 p-5 bg-white rounded-xl shadow-md w-52 ring-1 ring-slate-900/5 z-50"
+                : "hidden xl:flex gap-x-5 xl:gap-x-10 medium-15 ring-1 ring-slate-900/5 rounded-full p-1"}`} />
             </div>
 
             {/* Buttons Right side */}
-            <div className='flex-1 flex items-center justify-end gap-x-2 xs:gap:x-8'>
+            <div className='flex-1 flex items-center justify-end gap-x-2 xs:gap-x-8'>
                 {/* menu toggle */}
-                <FaBarsStaggered className='xl:hidden cursor-pointer text-xl' />
+                <FaBarsStaggered  onClick={toggleMenu} className='xl:hidden cursor-pointer text-xl' />
                 {/* Search icon */}
                 <FaSearch className='text-lg cursor-pointer'/>
                 {/* Cart */}
                 <Link to={'/'} className='flex relative'>
                     <TbBasket className='text-[27px]'/>
-                    <span className='bg-secondary text-white text-[12px] font-semibold 
+                    <span className='bg-secondary text-white text-[12px] font-semibold absolute
                     left-1.5 -top-3.5 flexCenter w-4 h-4 rounded-full 
                     shadow-md'>0</span>
                 </Link>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,18 +6,23 @@ const Navbar = ({containerStyles}) => {
     const navLinks = [
       {path: '/', title: 'Home'},
       {path: '/collection', title: 'Collection'},
-      {path: '/testimonial', title: 'Testimonial'},
-      {path: '/mailto:info@evostep.com', title: 'Contact'},
+      {path: '/testimonials', title: 'Testimonials'},
+      {path: 'mailto:info@evostep.com', title: 'Contact'},
     ]
   return (
-    <nav className={'${containerStyles}'}>
-      {navLinks.map((link)=>(
+    <nav className={`${containerStyles}`}>
+      {navLinks.map((link) => (
         <NavLink
-        key={link.title}
-        to={link.path}
-        // className={({isActive})=> '${isActive ? "active-link"}'}
+          key={link.title}
+          to={link.path}
+          className={({ isActive }) =>
+            `${isActive ? "active-link " : ""} px-3 py-2 rounded-full`
+          }
         >
-        {link.title}
+          <div className='flexCenter gap-x-1'>
+            {link.title}
+          </div>
+          
         </NavLink>
       ))}
     </nav>

0eb9a7ce603de9be997ed228469e8dc94de217c7
Senuka Chandunu <senukaacc@gmail.com>
2025-01-03 21:26:49 +0530

    Import & link images to product-page section

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
+import React, { useContext, useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { ShopContext } from '../context/shopContext';
+import { FaStar, FaStarHalfStroke, FaTruckFast } from 'react-icons/fa6';
+import { FaHeart } from 'react-icons/fa';
+import { TbShoppingBagPlus } from 'react-icons/tb';
 
 const Product = () => {
+
+  const { productId } = useParams();
+  const { products, currency } = useContext(ShopContext);
+  const [product, setProduct] = useState(null);
+  const [image, setImage] = useState("");
+  const [size, setSize] = useState("");
+
+  const fetchProductData = async () => {
+    const selectedProduct = products.find((item) => item._id === productId);
+    if (selectedProduct) {
+      setProduct(selectedProduct);
+      setImage(selectedProduct.image[0]);
+      console.log(selectedProduct);
+    }
+  }
+
+  useEffect(() => {
+    fetchProductData();
+  }, [productId, products]);
+
+  if (!product) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      Product
+      <div>
+        {/* Product Data */}
+        <div>
+          {/* Product Image */}
+            <div>
+              <div>
+                {product.image.map((item, i) => (
+                  <img key={i} src={item} alt='producImg' className='max-h-[89px] rounded-lg' />
+                ))}
+              </div>
+            </div>
+        </div>
     </div>
+  </div>
   )
 }
 

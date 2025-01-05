b63284224963f0fa51646b389c5fb60978550512
Senuka Chandunu <senukaacc@gmail.com>
2025-01-05 13:52:25 +0530

    Cart increment & decrement

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,81 +1,123 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { FaRegWindowClose } from 'react-icons/fa';
-import { FaMinus, FaPlus } from 'react-icons/fa6';
-import { ShopContext } from '../context/shopContext';
+import React, { useContext, useEffect, useState } from "react";
+import { FaRegWindowClose } from "react-icons/fa";
+import { FaMinus, FaPlus } from "react-icons/fa6";
+import { ShopContext } from "../context/shopContext";
+import Title from "../components/Title";
 
 const Cart = () => {
-    const { products, currency, cartItems, getCartCount, navigate } = useContext(ShopContext);
-    const [cartData, setCartData] = useState([]);
-    const [quantities, setQuantities] = useState({});
+  const { products, currency, cartItems, getCartCount, navigate } =
+    useContext(ShopContext);
+  const [cartData, setCartData] = useState([]);
+  const [quantities, setQuantities] = useState({});
 
-    useEffect(() => {
-        if (products.length > 0) {
-            const tempData = [];
-            const initialQuantities = {};
+  useEffect(() => {
+    if (products.length > 0) {
+      const tempData = [];
+      const initialQuantities = {};
 
-            for (const items in cartItems) {
-                for (const item in cartItems[items]) {
-                    if (cartItems[items][item] > 0) {
-                        tempData.push({
-                            _id: items,
-                            size: item,
-                            quantity: cartItems[items][item]
-                        });
-                        initialQuantities[`${items} ${item}`] = cartItems[items][item];
-                    }
-                }
-            }
-            setCartData(tempData);
-            setQuantities(initialQuantities);
+      for (const items in cartItems) {
+        for (const item in cartItems[items]) {
+          if (cartItems[items][item] > 0) {
+            tempData.push({
+              _id: items,
+              size: item,
+              quantity: cartItems[items][item],
+            });
+            initialQuantities[`${items} ${item}`] = cartItems[items][item];
+          }
         }
-    },[cartItems, products]);
+      }
+      setCartData(tempData);
+      setQuantities(initialQuantities);
+    }
+  }, [cartItems, products]);
 
-    const increment = (id, size) => {
-        const key = `${id}-${size}`;
-        const newValue = quantities[key] + 1;
-        setQuantities(prev => ({ ...prev, [key]: newValue }));
-        updateQuantity(id, size, newValue);
-      };
-      
-      const decrement = (id, size) => {
-        const key = `${id}-${size}`;
-        if (quantities[key] > 1) {
-          const newValue = quantities[key] - 1;
-          setQuantities(prev => ({ ...prev, [key]: newValue }));
-          updateQuantity(id, size, newValue);
-        }
-      };
+  const increment = (id, size) => {
+    const key = `${id}-${size}`;
+    const newValue = quantities[key] + 1;
+    setQuantities((prev) => ({ ...prev, [key]: newValue }));
+    updateQuantity(id, size, newValue);
+  };
 
-      return (
-        <div>
-          <div>
-            <div>
-              {/* Title */}
-              <Title title1={'Cart'} title2={'List'} title1Styles={'h3'} />
-              <h5> ({getCartCount()} Items)</h5>
-            </div>
-            {/* Container */}
-            <div>
-              {cartData.map((item, i) => {
-                const productData = products.find(
-                  (product) => product._id === item._id
-                );
-                const key = `${item._id}-${item.size}`
+  const decrement = (id, size) => {
+    const key = `${id}-${size}`;
+    if (quantities[key] > 1) {
+      const newValue = quantities[key] - 1;
+      setQuantities((prev) => ({ ...prev, [key]: newValue }));
+      updateQuantity(id, size, newValue);
+    }
+  };
 
-                return (
-                    <div key={i}>
-                        <div>
-                            <div>
-                                <img src={productData.image[0]} alt="productImg" className='w-16 sm:w-18 rounded' />
-                            </div>
-                        </div>
+  return (
+    <div>
+      <div className="bg-primary mb-16">
+        <div className="max-padd-container py-19">
+          {/* Title */}
+          <div className="flexStart gap-x-4">
+            <Title title1={"Cart"} title2={"List"} title1Styles={"h3"} />
+            <h5 className="medium-15 text-gray-30 relative bottom-1.5">
+              {" "}
+              ({getCartCount()} Items)
+            </h5>
+          </div>
+
+          {/* Container */}
+          <div className="mt-6">
+            {cartData.map((item, i) => {
+              const productData = products.find(
+                (product) => product._id === item._id
+              );
+              const key = `${item._id}-${item.size}`;
+
+              return (
+                <div key={i} className="rounded-lg bg-white p-2 mb-3">
+                  <div className="flex items-center gap-x-3">
+                    <div className="flex items-start gap-6">
+                      <img
+                        src={productData.image[0]}
+                        alt="productImg"
+                        className="w-16 sm:w-18 rounded"
+                      />
                     </div>
-                )
-              })}
-            </div>
+                    <div className="flex flex-col w-full">
+                      <div className="flexBetween">
+                        <h5 className="h5 !my-0 line-clamp-1">
+                          {productData.mame}
+                        </h5>
+                        <FaRegWindowClose className="cursor-pointer text-secondary" />
+                      </div>
+                      <p className="bold-14 my-0.5">{item.size}</p>
+                      <div className="flexBetween">
+                        <div className="flex items-center ring-1 ring-slate-900/5 rounded-full overflow-hidden bg-primary">
+                          <button
+                            onClick={() => decrement(item._id, item.size)}
+                            className="p-1.5 bg-white text-secondary rounded-full shadow-md"
+                          >
+                            <FaMinus className="text-sm" />
+                          </button>
+                          <p className="px-2">{quantities[key]}</p>
+                          <button
+                            onClick={() => increment(item._id, item.size)}
+                            className="p-1.5 bg-white text-secondary rounded-full shadow-md"
+                          >
+                            <FaPlus className="text-xs" />
+                          </button>
+                        </div>
+                        <h4 className="h4">
+                          {currency}
+                          {productData.price}
+                        </h4>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
-      )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Cart;
